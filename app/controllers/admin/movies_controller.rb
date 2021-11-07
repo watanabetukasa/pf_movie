d163c913f06cc0d7e75cca7d01f0dba2ed509f5f
watanabetukasa <tukasa533820@gmail.com>
2021-11-07 16:05:30 +0000

    [Start]管理者:タイトル追加機能

---
+++ app/controllers/admin/movies_controller.rb
@@ -0,0 +1,8 @@
+class Admin::MoviesController < ApplicationController
+  
+  def index
+  end
+  
+  
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  devise_for(
+    :admins,
+    path: 'admin',
+    module: 'devise'
+    )
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :admin do
+    root to: 'homes#top'
+    resources :movies, only:[:index,:new,:show,:edit,:create]
+  end
 end
